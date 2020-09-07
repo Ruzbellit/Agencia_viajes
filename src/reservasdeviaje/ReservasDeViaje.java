706215a71a0fd8c0bb5f8ec4fc034f670b33482e
Ruzbellit <ruzbellit.romero@correounivalle.edu.co>
2020-09-07 13:35:57 -0500

    Añadi contenido a infoReserva y otros cambio

--- src/reservasdeviaje/ReservasDeViaje.java
+++ src/reservasdeviaje/ReservasDeViaje.java
@@ -15,7 +15,7 @@ public class ReservasDeViaje {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code apllication logic here
     }
-    
+   
 }
